0546cb70d882259b0e1c8dd2515de3e4664adbb9
Shrishti Shah <shrishtishah2002@gmail.com>
2022-12-24 03:55:47 +0530

    finall

--- searchbutton/settings.py
+++ searchbutton/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ove)i%$x%(=p4*qogwi4ycg0613-alc6+s4h67!r_*yae=hu-4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -123,8 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT =  os.path.join(BASE_DIR, "assets")
-STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
