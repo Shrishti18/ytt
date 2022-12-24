8857d788cf5fc5e67313a695927877458f4fc254
Shrishti Shah <shrishtishah2002@gmail.com>
2022-12-24 21:06:18 +0530

    hi

--- searchbutton/wsgi.py
+++ searchbutton/wsgi.py
@@ -13,8 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 import sys
 
-DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
-sys.path.append(DJANGO_PATH)
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'searchbutton.settings')
 
